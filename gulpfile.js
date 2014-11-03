94b72b28b300ef16f78dee14f4d8054e676a7c2c
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2014-11-03 15:33:48 +0100

    minimist over gutil.env, express + browser-sync over gulp-webserver, minify-css over cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var webserver = require('gulp-webserver');
+var express = require('express');
+var browserSync = require('browser-sync');
 var gutil = require('gulp-util');
-var cssmin = require('gulp-cssmin');
+var minimist = require('minimist');
+var minifyCss = require('gulp-minify-css');
 var buffer = require('gulp-buffer');
 var uglify = require('gulp-uglify');
 
-var environment = gutil.env.environment || 'development';
+var server;
+var options = minimist(process.argv);
+var environment = options.environment || 'development';
 
 gulp.task('html', function() {
   return gulp.src('src/html/**/*.html')
@@ -23,7 +27,7 @@ gulp.task('styles', function() {
     .pipe(sass({
       sourceComments: environment === 'development' ? 'map' : false
     })).on('error', handleError)
-    .pipe(environment === 'production' ? cssmin() : gutil.noop())
+    .pipe(environment === 'production' ? minifyCss() : gutil.noop())
     .pipe(gulp.dest('dist/styles'))
     .pipe(reload());
 });
@@ -39,8 +43,10 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('server', function() {
-  return gulp.src('dist')
-    .pipe(webserver({ livereload: true }));
+  server = express();
+  server.use(express.static('dist'));
+  server.listen(8000);
+  browserSync({ proxy: 'localhost:8000' });
 });
 
 gulp.task('build', ['html', 'styles', 'scripts']);
@@ -59,8 +65,8 @@ function handleError(err) {
 }
 
 function reload() {
-  if (webserver.reload) {
-    return webserver.reload();
+  if (server) {
+    return browserSync.reload({ stream: true });
   }
 
   return gutil.noop();
